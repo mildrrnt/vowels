f0303383689a8d0858abc203b49281e9dca3824b
mildrrnt <woranita888@gmail.com>
2021-08-09 13:52:20 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vowels
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vowels)
---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('content').style.display = 'none';
+
+window.onload = event => {
+  document.getElementById('content').style.display = 'block';
+  document.getElementById('loading').style.display = 'none';
+};
